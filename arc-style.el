43245c150e89ff858bec9e7412ad6b071a787ab7
Lars T Hansen <larstha@uio.no>
2026-09-02 15:06:08 +0200

    Simpler Arc style

--- arc-style.el
+++ arc-style.el
@@ -1,24 +1,31 @@
 ;; Link to this from .dir-locals.el in the ARC root directory.
 
-((c++-mode . ((c-basic-offset 2)
-              (c-file-offsets (case-label . 0) (statement-case-intro . 4)
-		              (statement-block-intro . 2)
-		              (substatement-open . 0) (substatement-label . 0)
-		              (label . 0)
-		              (brace-list-intro first
-				                c-lineup-2nd-brace-entry-in-arglist
-				                c-lineup-class-decl-init-+ 2)
-		              (statement-cont . 2))
-              (c-comment-only-line-offset . 0)))
- (c-mode .  ((c-basic-offset 2)
-              (c-file-offsets (case-label . 0) (statement-case-intro . 4)
-		              (statement-block-intro . 2)
-		              (substatement-open . 0) (substatement-label . 0)
-		              (label . 0)
-		              (brace-list-intro first
-				                c-lineup-2nd-brace-entry-in-arglist
-				                c-lineup-class-decl-init-+ 2)
-		              (statement-cont . 2))
-              (c-comment-only-line-offset . 0))))
+((c++-mode . ((c-basic-offset . 2)
+              (indent-tabs-mode . nil)
+	      (fill-column . 100)))
+ (c-mode   . ((c-basic-offset . 2)
+              (indent-tabs-mode . nil)
+	      (fill-column . 100))))
+
+;; ((c++-mode . ((c-basic-offset 2)
+;;               (c-file-offsets (case-label . 0) (statement-case-intro . 4)
+;; 		              (statement-block-intro . 2)
+;; 		              (substatement-open . 0) (substatement-label . 0)
+;; 		              (label . 0)
+;; 		              (brace-list-intro first
+;; 				                c-lineup-2nd-brace-entry-in-arglist
+;; 				                c-lineup-class-decl-init-+ 2)
+;; 		              (statement-cont . 2))
+;;               (c-comment-only-line-offset . 0)))
+;;  (c-mode .  ((c-basic-offset 2)
+;;               (c-file-offsets (case-label . 0) (statement-case-intro . 4)
+;; 		              (statement-block-intro . 2)
+;; 		              (substatement-open . 0) (substatement-label . 0)
+;; 		              (label . 0)
+;; 		              (brace-list-intro first
+;; 				                c-lineup-2nd-brace-entry-in-arglist
+;; 				                c-lineup-class-decl-init-+ 2)
+;; 		              (statement-cont . 2))
+;;               (c-comment-only-line-offset . 0))))
 
 
